5dedf21c8008a8b973696b42cdf473e6b7f22769
Justin Watt <justincwatt@gmail.com>
2012-09-23 15:29:47 -0700

    Spaces to tabs

--- redirect-editor.php
+++ redirect-editor.php
@@ -28,69 +28,69 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 new Redirect_Editor_Plugin();
 class Redirect_Editor_Plugin {
 
-    public function __construct() {
+	public function __construct() {
 
-        add_action( 'admin_init', array( &$this, 'save_data' ) );
+		add_action( 'admin_init', array( &$this, 'save_data' ) );
 
-        add_action( 'admin_menu', array( &$this, 'add_admin_menu' ) );
+		add_action( 'admin_menu', array( &$this, 'add_admin_menu' ) );
 
-        add_action( 'pre_get_posts', array( &$this, 'redirect_editor' ) );
-    }
+		add_action( 'pre_get_posts', array( &$this, 'redirect_editor' ) );
+	}
 
-    public function add_admin_menu() {
-        add_options_page( 'Redirect Editor', 'Redirect Editor', 'manage_options', 'redirect-editor', array( &$this, 'admin_page' ) );
-    }
+	public function add_admin_menu() {
+		add_options_page( 'Redirect Editor', 'Redirect Editor', 'manage_options', 'redirect-editor', array( &$this, 'admin_page' ) );
+	}
 
-    public function admin_page() {
-        $redirects = $this->load_data();
-        require_once( 'form.php' );
-    }
+	public function admin_page() {
+		$redirects = $this->load_data();
+		require_once( 'form.php' );
+	}
 
-    // get saved data and transform into string
-    function load_data() {
-        $redirects_array = get_option( 'redirect_editor' );
-        $redirects = '';
-        foreach ( $redirects_array as $key => $value ) {
-            $redirects .= $key . ' ' . $value . "\n";
-        }
-        return $redirects;
-    }
+	// get saved data and transform into string
+	function load_data() {
+		$redirects_array = get_option( 'redirect_editor' );
+		$redirects = '';
+		foreach ( $redirects_array as $key => $value ) {
+			$redirects .= $key . ' ' . $value . "\n";
+		}
+		return $redirects;
+	}
 
-    // transform POSTed string data in array and save
-    // format: /2012/09/old-post-destination/ http://www.example.com/2012/09/new-post-destination/
-    function save_data() {
-        if ( !isset( $_POST['function'] ) || !check_admin_referer( 'redirect-editor' ) ) {
-            return false;
-        }
+	// transform POSTed string data in array and save
+	// format: /2012/09/old-post-destination/ http://www.example.com/2012/09/new-post-destination/
+	function save_data() {
+		if ( !isset( $_POST['function'] ) || !check_admin_referer( 'redirect-editor' ) ) {
+			return false;
+		}
 
-        if ( isset( $_POST['redirects']) ) {
-            # explode textarea on newline
-            $redirect_lines = explode( "\n", $_POST['redirects'] );
+		if ( isset( $_POST['redirects']) ) {
+			# explode textarea on newline
+			$redirect_lines = explode( "\n", $_POST['redirects'] );
 
-            # explode each line on space
-            $redirects_array = array();
-            foreach ( $redirect_lines as $redirect_line ) {
-                $redirect_line = explode( " ", preg_replace( '/\s+/', ' ', trim( $redirect_line ) ), 2 );
-                if ( count( $redirect_line ) != 2 )
-                    continue;
-                $redirects_array[$redirect_line[0]] = $redirect_line[1];
-            }
+			# explode each line on space
+			$redirects_array = array();
+			foreach ( $redirect_lines as $redirect_line ) {
+				$redirect_line = explode( " ", preg_replace( '/\s+/', ' ', trim( $redirect_line ) ), 2 );
+				if ( count( $redirect_line ) != 2 )
+					continue;
+				$redirects_array[$redirect_line[0]] = $redirect_line[1];
+			}
 
-            update_option( 'redirect_editor', $redirects_array );
-        }
+			update_option( 'redirect_editor', $redirects_array );
+		}
 
-        // we can redirect here because save_data is called in admin_init action
-        wp_redirect( admin_url( 'options-general.php?page=redirect-editor' ) );
-    }  
+		// we can redirect here because save_data is called in admin_init action
+		wp_redirect( admin_url( 'options-general.php?page=redirect-editor' ) );
+	}
 
-    // it all comes down to this
-    function redirect_editor( $query ) {
-        $request_url = $_SERVER["REQUEST_URI"];
-        $redirects = get_option( 'redirect_editor' );
+	// it all comes down to this
+	function redirect_editor( $query ) {
+		$request_url = $_SERVER["REQUEST_URI"];
+		$redirects = get_option( 'redirect_editor' );
 
-        if ( array_key_exists( $request_url, $redirects ) ) {
-            wp_redirect( $redirects[$request_url], 301 );
-            exit;            
-        }
-    }
+		if ( array_key_exists( $request_url, $redirects ) ) {
+			wp_redirect( $redirects[$request_url], 301 );
+			exit;
+		}
+	}
 }
